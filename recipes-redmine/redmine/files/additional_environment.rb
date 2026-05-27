f128ce654e8bf15473f3654c300e43f045c938de
Krzysztof Drobiński <krzysztof@kd-solutions.pl>
2026-05-27 16:53:40 +0200

    redmine: Add support for SSL
    Signed-off-by: Krzysztof Drobiński <krzysztof@kd-solutions.pl>

--- recipes-redmine/redmine/files/additional_environment.rb
+++ recipes-redmine/redmine/files/additional_environment.rb
@@ -6,3 +6,4 @@ log_dir = ENV.fetch('REDMINE_LOG_DIR', '@REDMINE_LOG_DIR@')
 config.paths['log'] = File.join(log_dir, "#{Rails.env}.log")
 config.cache_store = :file_store, File.join(writable_dir, 'tmp', 'cache')
 config.active_record.dump_schema_after_migration = false
+config.force_ssl = true
